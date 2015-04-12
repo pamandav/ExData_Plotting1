19e9d6b19e73f03c93ec9c2d7d121311d8a1419b
pamandav <pamandav@users.noreply.github.com>
2015-04-12 22:28:55 +0530

    Update plot2.R
    Added dplyr package

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+require(dplyr)
 input <- read.csv("exdata-data-household_power_consumption/household_power_consumption.txt", sep =";", header=TRUE,colClasses="character")
 input1 <- tbl_df(input)
 input2 <- filter(input1, Date=="1/2/2007" | Date =="2/2/2007")
